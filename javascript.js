6542c2041980ed9b09514021553faa9741362a97
Victor Esso <victoresso97@gmail.com>
2022-11-09 23:52:22 +0100

    another one

--- javascript.js
+++ javascript.js
@@ -9,30 +9,43 @@ $(document).ready(function(){
         })
         //$('#question-container').unmark();
     }
+    const performSearch = () =>{
+        let searchValue = $('#search-key').val().trim();
+        if(searchValue.length){
+            markArray = searchValue.split(' ');
+            //$('#question-container').mark(markArray);
+            $('#question-container').children().children('.question').each(function(){
+                searchHaystack = '';
+                searchHaystack += $(this).children('p').text().trim()+' ';
 
-$('#search-key').keyup(function(){
-    resetAccordion();
-    let searchValue = $(this).val().trim();
-    if(searchValue.length){
-        markArray = searchValue.split(' ');
-        //$('#question-container').mark(markArray);
-        $('#question-container').children().children('.question').each(function(){
-            searchHaystack = '';
-            searchHaystack += $(this).children('p').text().trim()+' ';
+                $(this).children('p').mark(markArray);
 
-            $(this).children('p').mark(markArray);
-
-            if(!searchDeep(searchValue,searchHaystack)){
-                $(this).css('display','none');
-            }else{
-                console.log('yes')
-            }
+                if(!searchDeep(searchValue,searchHaystack)){
+                    $(this).css('display','none');
+                }else{
+                    console.log('yes')
+                }
+                
+            })
+        }else{
+            resetAccordion();
             
-        })
-    }else{
-        resetAccordion();
-        
+        }
     }
+
+$('#go-btn').click(function(){
+    resetAccordion();
+    performSearch();
+    
+})
+
+$('#search-key').blur(function(){
+    resetAccordion();
+    performSearch();
+})
+$('#search-key').focus(function(){
+    resetAccordion();
+    performSearch();
 })
 
 
